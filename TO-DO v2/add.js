3faa280f20edbcfe50e2846aeb2ab74bfa78819f
chriscod91 <80732087+chriscod91@users.noreply.github.com>
2022-08-03 20:41:41 -0700

    Add files via upload
    just a simple to do app

---
+++ TO-DO v2/add.js
@@ -0,0 +1,22 @@
+
+
+//function saveTodo(){
+    
+    //let text = $("#todoItems").val();// get the text from the input field
+    //console.log('saving:' + text);
+
+    //let txt = "<div><h1>" + text + "</h1></div>";
+    //$("#list").append(txt);
+//}
+
+ //function init(){
+     //console.log('todo');
+     //load data 
+
+     //hook event
+     //$('#btnSave').click( saveTodo );
+ //}
+
+ //window.onload = init;
+
+ 
